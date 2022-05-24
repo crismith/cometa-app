2028fb3f7363a4a7d9224850f269b5a70599d59c
Cristhian Smith <cristian.ismen1@gmail.com>
2022-05-24 00:18:42 -0500

    header y ruta

--- src/api/service.js
+++ src/api/service.js
@@ -2,18 +2,17 @@ const API_URL = "http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1";
 const requestOptions = {
   headers: { 
     "hash": "OcJn4jYChW",
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Origin': '*',
-    "Access-Control-Allow-Origin": "*"
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "X-Requested-With"
   }
 };
 
 let payments = [];
 
 export async function getStudent() {
+  let ruta = API_URL + '/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/';
   try {
-    const response = await fetch(`${API_URL}/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/`, requestOptions);
+    const response = await fetch(ruta, requestOptions);
     const data = await response.json();
     return data;
   } catch (error) {
@@ -22,8 +21,9 @@ export async function getStudent() {
 }
 
 export async function getStudentOrders() {
+  let ruta = API_URL + '/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/orders';
   try {
-    const response = await fetch(`${API_URL}/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/orders`, requestOptions);
+    const response = await fetch(ruta, requestOptions);
     const data = await response.json();
     payments = data; 
     return data;
